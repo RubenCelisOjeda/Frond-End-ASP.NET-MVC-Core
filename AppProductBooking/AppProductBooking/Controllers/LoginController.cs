33ec7a1f1f893fb285bb598d9d709feb5dd08198
Ruben Celis <sistemas.celis@gmail.com>
2023-04-08 12:10:12 -0500

    The login page was created

---
+++ AppProductBooking/AppProductBooking/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppProductBooking.Controllers
+{
+    public class LoginController : Controller
+    {
+        #region [Properties]
+
+        #endregion
+
+        #region [Constructor]
+
+        #endregion
+
+        #region [ActionResult]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
