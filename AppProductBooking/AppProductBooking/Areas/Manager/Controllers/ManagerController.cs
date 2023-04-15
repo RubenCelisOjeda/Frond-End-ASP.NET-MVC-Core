77663caa7a248e809765e81e5741e8fa4b5d4cef
Ruben Celis <sistemas.celis@gmail.com>
2023-04-15 15:26:05 -0500

    Avanze de la reacion de controlador de manager

---
+++ AppProductBooking/AppProductBooking/Areas/Manager/Controllers/ManagerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppProductBooking.Areas.Manager.Controllers
+{
+    public class ManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
